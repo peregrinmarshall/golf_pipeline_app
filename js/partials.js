790b68086335152dddaad8e7dd4fa8e62ecb7f9d
Peregrin Marshall <peremarshall@gmail.com>
2013-08-05 09:14:52 -0600

    trying different path for images refere

--- js/partials.js
+++ js/partials.js
@@ -1,6 +1,6 @@
 $(document).ready( function()
 {
-  $(".header").html('<a href="index.html" data-rel="back" data-theme="none"><img src="../img/button_back.png" /></a><center><a href="#page_home" data-theme="none"><img src="../img/logo.png" class="mini_logo" /></a></center><a href="#page_options" data-theme="none"><img src="../img/icon_gear.png" /></a><div class="nav_strip"><ul><center><li>Profile</li><li>Home</li><li>Search Tee Times</li></center></ul><br class="clear" /><div id="white_arrow"><center>&#9650;</center></div></div>');
+  $(".header").html('<a href="index.html" data-rel="back" data-theme="none"><img src="./img/button_back.png" /></a><center><a href="#page_home" data-theme="none"><img src="../img/logo.png" class="mini_logo" /></a></center><a href="#page_options" data-theme="none"><img src="../img/icon_gear.png" /></a><div class="nav_strip"><ul><center><li>Profile</li><li>Home</li><li>Search Tee Times</li></center></ul><br class="clear" /><div id="white_arrow"><center>&#9650;</center></div></div>');
 
   $(".footer").html('<div data-role="navbar" data-iconpos="top"><a href="#page_social" data-icon="arrow-u" data-transition="slideup" data-theme="a">Notifications</a></div>');
 });
